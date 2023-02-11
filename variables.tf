51a9919a1b03dcef5530e7522d794a8c7fe772b7
JATINKUMAR1008 <97216852+JATINKUMAR1008@users.noreply.github.com>
2023-02-12 01:43:02 +0530

    o

--- variables.tf
+++ variables.tf
@@ -1,34 +1,17 @@
-<<<<<<< HEAD
 variable "rgname" {
-  type = string
+  type        = string
   description = "resource group name"
 }
 
 variable "location" {
-  type = string
-  default = "centralindia"
+  type    = string
+  default = "canadacentral"
 }
+
 variable "service_principal_name" {
   type = string
-
 }
+
 variable "keyvault_name" {
   type = string
-=======
-variable "rgname" {
-  type = string
-  description = "resource group name"
-}
-
-variable "location" {
-  type = string
-  default = "centralindia"
-}
-variable "service_principal_name" {
-  type = string
-
-}
-variable "keyvault_name" {
-  type = string
->>>>>>> 0df3c40f832c0eec7a424b433f7d77254823cdef
 }
